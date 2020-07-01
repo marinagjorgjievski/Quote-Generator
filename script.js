de8306652601355c302ae43ee69f10779df6258f
Marina <48449555+marinagjorgjievski@users.noreply.github.com>
2020-07-01 15:21:49 +0200

    manipulating DOM with js

--- script.js
+++ script.js
@@ -1,17 +1,50 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 //Get Quote From API
 
 async function getQuote() {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-    try {
-        const response = await fetch(proxyUrl + apiUrl);
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        getQuote();
-        console.log('whoops, no quote', error);
+  const proxyUrl = 'https://salty-crag-68937.herokuapp.com/'
+  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+
+    //If Author is blank, add 'Unknown'
+    if (data.quoteAuthor === '') {
+        authorText.innerText = 'Unknown';
+    } else {
+        authorText.innerText = data.quoteAuthor;
     }
+
+    //Reduce font size for long quotes
+    if(data.quoteText.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+
+    quoteText.innerText = data.quoteText;
+
+  } catch (error) {
+    getQuote();
+  }
 }
+
+//Tweet Quote
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twittertUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twittertUrl, '_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click',getQuote);
+twitterBtn.addEventListener('click',tweetQuote);
 
 //On Load
 getQuote();
